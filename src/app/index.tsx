8564d3fab65656fbf8afca4e9eb6183c822b2617
natehubbit <natehubbit1001@gmail.com>
2021-09-03 16:43:10 +0000

    Cached state with local storage implementation

---
+++ src/app/index.tsx
@@ -0,0 +1,43 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+
+import { STATE_CACHE } from "../common/constants";
+import Panel from "../components/Panel";
+import { useLoader } from "../hooks/useLoader";
+import { useProducts } from "../hooks/useProducts";
+import { productActions } from "../redux/slice/products";
+import { AppDispatch, store } from "../redux/store";
+import { getStoreItem, storeItem } from "../utils";
+import styles from "./style.module.scss";
+
+const App = () => {
+  const dispatch: AppDispatch = useDispatch();
+  const products = useProducts();
+  const loading = useLoader();
+  const getCachedProducts = () => {
+    const data = getStoreItem(STATE_CACHE);
+    return data ? data.products : null;
+  };
+  useEffect(() => {
+    const cachedProducts = getCachedProducts();
+    const unsubscribe = store.subscribe(() => {
+      const state = store.getState();
+      storeItem(STATE_CACHE, state);
+    });
+    if (cachedProducts) {
+      dispatch(productActions.set(cachedProducts));
+    } else {
+      dispatch(productActions.populate());
+    }
+    return () => unsubscribe();
+    // eslint-disable-next-line
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      <Panel products={products} loading={loading} />
+    </div>
+  );
+};
+
+export default App;
